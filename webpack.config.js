cae4e6a14054eeead21845a4e61b4f14c13d5eca
MacBook <http_wenwen@live.com>
2020-02-21 18:34:27 +0800

    load-css

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
     module: {
         rules: [
             {
