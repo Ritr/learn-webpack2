560afe51bd45be334f1813a9eb65d1c2cb47eed3
MacBook <http_wenwen@live.com>
2020-02-27 23:37:23 +0800

    预取 预加载

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import Div from './div.js';
-var div = Div();
-document.body.appendChild(div);
-div.innerHTML = '点击加载input.js';
-div.onclick = getInput;
 
-async function getInput(){
-    const { default: Input } = await import(/* webpackChunkName:"input222" */'./input.js');
-    document.body.appendChild(Input());
+var button = document.createElement('button');
+button.innerText = '加载DIV模块'
+button.onclick = getDiv;
+document.body.appendChild(button);
+async function getDiv() {
+    const { default: Div } = await import(/* webpackChunkName:"div",webpackPrefetch:true */'./div.js');
+    console.log('prefetch');
 }
+
+
+async function getInput() {
+    const { default: Input } = await import(/* webpackChunkName:"input",webpackPreload:true */'./input.js');
+    Input();
+    console.log('preload');
+}
+
+getInput();
 
--- src/input.js
+++ src/input.js
@@ -1,6 +1,5 @@
 function Input() {
     var input = document.createElement('input');
-    console.log('动态引入成功');
     return input;
 }
 export default Input;
