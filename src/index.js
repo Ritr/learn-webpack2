d40161ee1e691ce3c5bbf7f4ea64f26640a094ad
MacBook <http_wenwen@live.com>
2020-03-01 21:16:22 +0800

    treeshaking

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import Input from './input.js';
-import Div from './div.js';
-import './style.css';
-var input = Input();
-var div = Div();
-document.body.appendChild(input);
-document.body.appendChild(div);
-
-//错误代码
-var id = document.getElementById('id');
-console.log(id.innerHTML);
-
-console.log(module.hot);
-if (module.hot) {
-    module.hot.accept('./div.js', function () {
-        console.log('div模块热更新');
-        document.body.removeChild(div);
-        document.body.appendChild(Div());
-    });
-}
+import { squre } from './math';
+var res = squre(2);
+console.log(res);
